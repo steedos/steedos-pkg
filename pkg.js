a41bcc754ae516a548ee6ff8cba2e28efc6a4b80
sunhaolin <sunhaolin@hotoa.com>
2021-01-15 17:41:36 +0800

    package script

--- pkg.js
+++ pkg.js
@@ -5,8 +5,9 @@ const path = require('path');
 const VERSION = require('steedos-server/package.json').version;
 const ossFold = process.env.STEEDOS_OSS_QUICK_INSTALL_FOLD;
 console.log('ossFold: ', ossFold);
-// const platforms = ['macos', 'win', 'linux'];
-const platforms = ['macos'];
+const platforms = ['macos', 'win', 'linux'];
+// const platforms = ['macos'];
+const targets = { 'macos': 'node12-macos-x64', 'win': 'node12-win-x64', 'linux': 'node12-linux-x64' }
 
 let cwd = process.cwd();
 let pkgPath = path.join(cwd, '../../pkg');
@@ -20,16 +21,18 @@ for (const os of platforms) {
     console.log(`${os}: osDistPath: `, osDistPath);
 
     console.log(`${os}: pkg start!`);
-    execSync(`pkg ${cwd} --options max_old_space_size=4096 --out-path=${osDistPath} --targets node12-macos-x64`);
+    execSync(`pkg ${cwd} --options max_old_space_size=4096 --out-path=${osDistPath} --targets ${targets[os]}`);
     console.log(`${os}: pkg done!`);
-    console.log(`${os}: zip start!`);
 
-    execSync(`zip -r ${osPath}/steedos-mini-macos-${VERSION}.zip ${osDistPath}`);
+    console.log(`${os}: zip start!`);
+    execSync(`cd ${osPath} && zip -r steedos-mini-${os}-${VERSION}.zip steedos-mini`);
     console.log(`${os}: zip done!`);
 
     console.log(`${os}: upload to aliyun!`);
-    execSync(`/usr/bin/ossutil --force cp ${osPath}/steedos-mini-macos-${VERSION}.zip ${ossFold}`);
-    console.log(`${os}: upload ${os} done!`);
+    execSync(`/usr/bin/ossutil --force cp ${osPath}/steedos-mini-${os}-${VERSION}.zip ${ossFold}`);
+    console.log(`${os}: upload done!`);
+
+    execSync(`cd ${cwd}`);
 }
 
 
